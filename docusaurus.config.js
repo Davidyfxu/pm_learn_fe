e80323fe769da5d272a956005cd675289b534be1
davidyfxu <82054510@qq.com>
2022-06-19 23:40:38 +0800

    feat: start to deploy2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: '给产品经理讲前端',
   tagline: 'Let PMs learn FE',
-  url: 'Davidyfxu.github.io',
+  url: 'https://davidyfxu.github.io/pm_learn_fe/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Davidyfxu', // Usually your GitHub org/user name.
   projectName: 'Davidyfxu.github.io', // Usually your repo name.
   deploymentBranch: 'main',
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
