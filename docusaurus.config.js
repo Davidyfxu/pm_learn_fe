d64a60e881d7776484682a92f5e6c29d81e83d67
davidyfxu <82054510@qq.com>
2022-06-19 23:44:16 +0800

    fix: deploy bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Davidyfxu', // Usually your GitHub org/user name.
